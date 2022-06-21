0e265b3ee535065933f033048b6b2ba585062354
Pedro Mack Navarro <pemnavarro@hotmail.com>
2022-06-21 09:25:47 -0300

    Criação componente listar carros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RegisterCarsComponent } from './register-cars/register-cars.component';
+import { ListCarsComponent } from './list-cars/list-cars.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RegisterCarsComponent
+    RegisterCarsComponent,
+    ListCarsComponent
   ],
   imports: [
     BrowserModule,
