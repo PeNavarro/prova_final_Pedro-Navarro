77294db54670337f7e1a91d2e7d3780e4712c70c
Pedro Mack Navarro <pemnavarro@hotmail.com>
2022-06-21 09:26:27 -0300

    Configuração das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterCarsComponent } from './register-cars/register-cars.component'
+import { ListCarsComponent } from './list-cars/list-cars.component'
 
-
+const routes: Routes = [
+  {path: 'register', component: RegisterCarsComponent},
+  {path: 'list', component: ListCarsComponent}
+]
 
 @NgModule({
   declarations: [],
   imports: [
+    RouterModule.forRoot(routes),
     CommonModule
-  ]
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
